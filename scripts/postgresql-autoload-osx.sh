494b48defccb04e65f72a324b18a53ec54323449
Devon Peticolas <devon@Devons-MacBook-Pro.local>
2012-09-01 17:21:41 -0400

    updated script to init and start db as well

--- scripts/postgresql-autoload-osx.sh
+++ scripts/postgresql-autoload-osx.sh
@@ -1,3 +1,10 @@
+# init the database
+initdb /usr/local/var/postgres
+
+# set it up to lauch on startup
 launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 cp /usr/local/Cellar/postgresql/9.1.4/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/
 launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+
+# start the db
+pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
