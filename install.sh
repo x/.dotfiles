4d1fd3f0f3623f482e4bef3343f038dd773f8819
Devon Peticolas <devon@chartbeat.com>
2016-05-30 16:26:28 -0400

    simplify install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 
-ignore=( scripts backups install.sh )
 DIR=~/.dotfiles
 
-cd $DIR
-for filename in *
-do
-    # check if the file should be ignored
-    shouldIgnore=false
-    for ignorename in ${ignore[@]}
-    do
-        if [[ $filename == $ignorename ]]
-        then
-            shouldIgnore=true
-        fi
-    done
-    
-    # if you shouldn't ignore, and it's not already linked
-    if [ $shouldIgnore == false -a ! -L ~/.$filename ]
-    then
-
-        # move old versions moved to backup dir
-        if [ -e ~/.$filename ]
-        then
-            echo ${filename} moved to ${DIR}/backups/${filename}
-            mv ~/.$filename $DIR/backups/
-        fi
-        
-        # create the link
-        echo new link ~/.${filename} to ${DIR}/backups/${filename}
-        ln -s $DIR/$filename ~/.$filename
-    fi
-done
+# install shit
+ln -s $DIR/bashrc    ~/.bashrc
+ln -s $DIR/vimrc     ~/.vimrc
+ln -s $DIR/tmux.conf ~/.tmux.conf
+ln -s $DIR/gitconfig ~/.gitconfig
+ln -s $DIR/xvimrc    ~/.xvimrc
 
 # source bashrc
 source ~/.bashrc
